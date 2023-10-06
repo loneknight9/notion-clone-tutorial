6ecfdfa2f2c9820423fd80eb76e63c19f2327e6d
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-10-07 00:29:14 +0200

    01: setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <div>
-      <Button className="bg-emerald-500 text-white">Delete</Button>
+      <Button className="bg-emerald-500 text-white">
+        Delete
+      </Button>
     </div>
   );
 }
