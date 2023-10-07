4c1d92b9a717b6f9c1a9d16d52fb0ebb5eb89e18
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-10-07 21:59:38 +0200

    03: landing

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-  return ( 
-    <div>
-      Login page!
-    </div>
-   );
-}
- 
-export default RegisterPage;
--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div>
-      <Button className="bg-emerald-500 text-white">Delete</Button>
-    </div>
-  );
-}
