af7730d03f32be45ac673c64649e45f3f56b08d2
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-10-08 22:30:52 +0200

    06: user-item

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return ( 
-    <div>
-      This is a protected DocumentsPage
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
    );
 }
