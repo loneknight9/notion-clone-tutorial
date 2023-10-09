a285a2381f55c86129848186bef06ac06e7c570f
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-10-09 23:01:09 +0200

    12: search

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -22,6 +22,7 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@/components/ui/popover";
+import { useSearch } from "@/hooks/use-search";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
@@ -29,6 +30,7 @@ import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -150,7 +152,7 @@ export const Navigation = () => {
             label="Search"
             icon={Search}
             isSearch
-            onClick={() => {}}
+            onClick={search.onOpen}
           />
           <Item
             label="Settings"
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 
 import { Spinner } from "@/components/spinner";
+import { SearchCommand } from "@/components/search-command";
 
 import { Navigation } from "./_components/navigation";
 
@@ -30,6 +31,7 @@ const MainLayout = ({
     <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
         {children}
       </main>
     </div>
