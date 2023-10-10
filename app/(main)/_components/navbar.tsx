134c8850f1af99ebf367d256eee0dfbf9b46d09b
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-10-10 23:34:10 +0200

    15: banner

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,6 +8,8 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -26,8 +28,11 @@ export const Navbar = ({
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     )
   }
@@ -46,10 +51,16 @@ export const Navbar = ({
             className="h-6 w-6 text-muted-foreground"
           />
         )}
-        <div className="flex items-center justify-between wf-ull">
+        <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && (
+        <Banner documentId={document._id} />
+      )}
     </>
   )
 }
